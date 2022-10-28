6dc0dad73af668564ae95d208dc733afaf8599de
Nathan Clevenger <nateclev@gmail.com>
2022-10-28 04:37:12 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -24,6 +24,13 @@ export const examples = {
   listItems: 'https://templates.do/worker',
 }
 
+
+    // Fetch YAML of Categories, transform to JSON
+    // Loop through categories and APIs and generate markdown
+    // ### Core:
+    // - [APIs.do: Hypermedia-driven APIs](https://apis.do) ([API](https://apis.do/api), [Docs](https://apis.do/docs), [Repo](https://github.com/drivly/apis.do), [Roadmap](https://github.com/drivly/apis.do/issues/1), [Issues](https://github.com/drivly/apis.do/issues))
+    // return new Response('') 
+
 export default {
   fetch: async (req, env) => {
     const { user, hostname, pathname, rootPath, pathSegments, query } = await env.CTX.fetch(req).then(res => res.json())
